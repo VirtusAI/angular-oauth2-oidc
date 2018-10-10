bda6f9744d0d07c4d3e7419ef54a603fd95af0fc
lpfeup <lpfeup@users.noreply.github.com>
2018-10-10 15:19:44 +0100

    do not validate issuer on processIdToken if skipIssuerCheck is true. update package.json version to 4.0.4-rc.1

--- projects/lib/src/oauth-service.ts
+++ projects/lib/src/oauth-service.ts
@@ -1497,7 +1497,7 @@ export class OAuthService extends AuthConfig {
             return Promise.reject(err);
         }
 
-        if (claims.iss !== this.issuer) {
+        if (!this.skipIssuerCheck && claims.iss !== this.issuer) {
             const err = 'Wrong issuer: ' + claims.iss;
             this.logger.warn(err);
             return Promise.reject(err);
